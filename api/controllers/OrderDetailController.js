1d321106de73dbfce0375e1b4cb2d5c9f7004268
Nuno <e_npgr@yahoo.com>
2015-08-04 21:08:27 -0430

    Crud5: OrderDetail

---
+++ api/controllers/OrderDetailController.js
@@ -0,0 +1,11 @@
+/**
+ * OrderDetailController
+ *
+ * @description :: Server-side logic for managing orderdetails
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/OrderDetail.js
@@ -0,0 +1,42 @@
+/**
+* OrderDetail.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	
+	migrate: 'alter',
+  attributes: {
+	//_title:'Detalle Pedido',
+	//_unique: ['order', 'product'],
+	id: {
+		//description: 'Id',
+		type: "integer",
+		max: 9999999,
+		min: 1,
+		primaryKey: true,
+		autoIncrement: true,
+		unique: true
+	},
+	order: {
+		//description: 'Pedido',
+		type: 'integer'
+	},
+	product: {
+		//description: 'Producto',
+		//key: id,
+		//key_type: 'integer',
+		//display: 'name',
+		//type: 'string',
+		model: 'Product',
+		required: true
+	},
+	quantity: {
+		//description: 'Cantidad',
+		type: 'integer'
+	}
+  }
+};
+
