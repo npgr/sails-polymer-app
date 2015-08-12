29804af5c78a64485dcea6a07da12aea49589561
Nuno <e_npgr@yahoo.com>
2015-08-11 22:16:47 -0430

    Crud5 FunctionAttribute:

--- api/models/FunctionAttribute.js
+++ api/models/FunctionAttribute.js
@@ -38,33 +38,36 @@ module.exports = {
 		//description: 'Attribute',
 		//key: id,
 		//key_type: 'integer',
-		//display: 'name',
+		//display: 'attribute',
 		//type: 'string',
 		model: 'Attribute'
 	},
 	display: {
-		//description: 'Attribute',
+		//description: 'Display',
 		type: 'string',
 		enum: ['Yes', 'ReadOnly', 'No']
 		//,enumdes: ['Yes', 'ReadOnly', 'No']
 	}
 //End Attributes
-	/*,toJSON: function () {
-      var obj = this.toObject();
+	,toJSON: function () {
+		var obj = this.toObject();
 
-      var func = {id: obj.functionx.id, name: obj.functionx.name, model: obj.functionx.model }
-	  var attr = {id: obj.attribute.id, name: obj.attribute.attribute, model: obj.attribute.model }
-	  
-	  delete obj.functionx;
-	  delete obj.attribute;
-	  delete obj.createdAt;
-	  delete obj.updatedAt;
-	  
-	  obj.functionx = func
-	  obj.attribute = attr
-
-      return obj;
-    }*/
+		var mdl = {id: obj.model.id, name: obj.model.name}
+		var func = {id: obj.functionx.id, name: obj.functionx.name}
+		var attr = {id: obj.attribute.id, attribute: obj.attribute.attribute}
+		
+		delete obj.model
+		delete obj.functionx
+		delete obj.attribute
+		delete obj.createdAt
+		delete obj.updatedAt
+		
+		obj.model = mdl
+		obj.functionx = func
+		obj.attribute = attr
+	
+		return obj;
+    } 
   }
 };
 
