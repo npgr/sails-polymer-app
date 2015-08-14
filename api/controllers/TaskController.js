91d21a5123b9f530d486ce3396367e0e952a0156
Nuno <e_npgr@yahoo.com>
2015-08-14 18:17:32 -0430

    TaskController: bytype, bystatus, byprioridad, byrequester, byresponsable

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -20,6 +20,41 @@ module.exports = {
 			.exec(function(err, data){
 				res.render("Task/list", {data: JSON.stringify(data)})
 			})
+	},
+	bytype: function(req, res) {
+		Task.query('SELECT type, count(*) FROM task group by type order by count', function(err, data) {
+			if (err) return res.serverError(err);
+			//return res.ok(results);
+			return res.json(data.rows)
+		});
+	},
+	bystatus: function(req, res) {
+		Task.query('SELECT status, count(*) FROM task group by status order by count', function(err, data) {
+			if (err) return res.serverError(err);
+			//return res.ok(results);
+			return res.json(data.rows)
+		});
+	},
+	byprioridad: function(req, res) {
+		Task.query('SELECT prioridad, count(*) FROM task group by prioridad order by count', function(err, data) {
+			if (err) return res.serverError(err);
+			//return res.ok(results);
+			return res.json(data.rows)
+		});
+	},
+	byrequester: function(req, res) {
+		Task.query('SELECT requester, count(*) FROM task group by requester order by count', function(err, data) {
+			if (err) return res.serverError(err);
+			//return res.ok(results);
+			return res.json(data.rows)
+		});
+	},
+	byresponsable: function(req, res) {
+		Task.query('SELECT responsable, count(*) FROM task group by responsable order by count', function(err, data) {
+			if (err) return res.serverError(err);
+			//return res.ok(results);
+			return res.json(data.rows)
+		});
 	}
 };
 
