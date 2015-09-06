9ff41441d5b0dfc9920322819cba633a8b0b36b9
Nuno <e_npgr@yahoo.com>
2015-09-06 18:20:28 -0430

    Order -> OrderDetail

--- api/controllers/OrderDetailController.js
+++ api/controllers/OrderDetailController.js
@@ -7,13 +7,12 @@
 
 module.exports = {
 	list : function (req, res) {
-		OrderDetail.find()
-			.exec(function(err, data){
-				res.locals.resources = req.session.resources
-				res.locals.user = {user: req.session.user, name: req.session.username}
-				res.locals.data = JSON.stringify(data)
-				res.view("OrderDetail/list")
-			})
+		//console.log("Header data: ",req.headers['app-post-data'])		
+		res.locals.resources = req.session.resources
+		res.locals.user = {user: req.session.user, name: req.session.username}
+		res.locals.data = []
+		res.locals.order = JSON.stringify(req.body)
+		res.view("OrderDetail/list")
 	}
 };
 
