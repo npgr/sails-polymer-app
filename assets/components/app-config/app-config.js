b8cd672f57cb7a61671032eba1c3f49639162631
Nuno <e_npgr@yahoo.com>
2015-10-13 00:10:56 -0430

    app-config: Google_Analytics_id()

--- assets/components/app-config/app-config.js
+++ assets/components/app-config/app-config.js
@@ -11,3 +11,8 @@ function config_base_url() {
 function config_language() {
 	return "Español"
 }
+
+function Google_Analytics_id() {
+	return 'UA-68195339-1'
+	// If this does not exist, Return false
+}
